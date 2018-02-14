54e4ab9b16463882d0fa6df90de42440c88a7d5c
Benjamin Meudre <bmeudre@gmail.com>
2018-02-14 20:45:59 +0100

    AWS DeepLens Challenge

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# deeplens-asl
-American Sign Language detection with AWS DeepLens
+# AWS DeepLens Challenge
+
+The future is not in keyboards and mice. Everyone agrees on the fact that interaction with computers will mainly use voice in the coming years. Siri, Cortana, Google Home, and, of course, Alexa (and Alexa for Business) are all examples of what the future may be. Even Werner Vogels thinks voice interaction will be the new traditional interaction (Nov’ 2017).
+
+However, if nearly everybody can speak to a computer, removing the need for literacy (mandatory with a keyboard), some people in the society are still put apart: those who can’t speak. Whether they are deaf-and-dumb, or they don’t know the right languages (e.g. Foreigners or immigrates), they cannot interact with conversational agents.
+
+The Deeplens may help them understand those who can’t speak, thanks to the American Sign Language (or some variants). The processing of video flow and a deep learning model can leverage the Deeplens as a new interface for them, translating ASL to written / spoken English in real time, so as to interact with computers.
+
+Our participation at the AWS DeepLens Challenge shows it is possible by understanding the ASL alphabet. We have chosen the alphabet because it is mainly composed of static hands positions.
+Our model is homemade, based on a Sagemaker vision model, fine trained on a specific dataset we have made during the challenge. We have removed ‘j’ and ‘z’ letters, as they require movement, and added special signs, up to you to find them ;)
+
+## Demo video
+
+Video available on [Youtube](https://www.youtube.com/watch?v=5VAKcQtoELo)
+
+## How to use it
+
+### Deployment
+
+The model is located in the *model* folder and the Lambda function handler is located in the *lambda* folder. To use it, you just have to follow the same steps as described in the [AWS documentation](https://docs.aws.amazon.com/deeplens/latest/dg/deeplens-train-model.html) starting step 4.
+
+Once deployed on the DeepLens, you will have to open the Project Stream to see the inference results and try to create sentences!
+
+### Interface
+
+The actual detected letter is shown in the top left corner (letter and confidence). If there is nothing displayed, it means that no letter was recognized with more than 40% confidence.
+
+On the bottom of the image you can see your message as it is written. Each letter appears if the same letter is recognized during a sufficient time.
+
+To make a whitespace: wait a few seconds with nothing recognized.
+To reset the message: wait longer ;)
+
+Be careful:
+* Results are better if the background is white-ish, and there is nothing in the DeepLens field of view.
+* Try to do the signs only in the field (no face / body / ...).
+* There is a small delay between your movement and the camera feedback.
+
+Here is the ASL alphabet that you can use:
+![ASL alphabet](images/asl-alphabet.png)
+
+## How we made it
+
+The dataset was made with a specific Lambda function that runs on the DeepLens (see "Snapshot sub-project" below for more information).
+
+We have taken different pictures with different people for each letter of the alphabet (except 'j' and 'z') and the special signs.
+
+The training was done using Amazon SageMaker, with transfer learning on a squeezenet with 18 layers. It converges in 4 epochs to a satisfaying result.
+
+Find below the architecture schema:
+![ASL inference](images/inference.png)
+
+## The Snapshot sub-project
+
+This project consists in using the DeepLens to generate the dataset. The camera is responding to user clicking on a button to take a snapshot and classifying the image right away.
+
+It uses a frontend hosted on S3, authenticating with Cognito and communicating with the DeepLens through IoT topics. Once images arrive in S3, they are automatically optimized for SageMaker training jobs.
+
+Find below the architecture schema:
+![ASL Snapshot](images/snapshot.png)
---
+++ lambda/greengrassHelloWorld.py
@@ -0,0 +1,145 @@
+import os
+import greengrasssdk
+import awscam
+import mo
+import cv2
+from threading import Thread
+
+client = greengrasssdk.client('iot-data')
+iot_topic = '$aws/things/{}/infer'.format(os.environ['AWS_IOT_THING_NAME'])
+client.publish(topic=iot_topic, payload="Starting...")
+
+jpeg = None
+Write_To_FIFO = True
+
+class FIFO_Thread(Thread):
+    def __init__(self):
+        ''' Constructor. '''
+        Thread.__init__(self)
+
+    def run(self):
+        fifo_path = "/tmp/results.mjpeg"
+        if not os.path.exists(fifo_path):
+            os.mkfifo(fifo_path)
+        f = open(fifo_path,'w')
+        client.publish(topic=iot_topic, payload="Opened Pipe")
+        while Write_To_FIFO:
+            try:
+                f.write(jpeg.tobytes())
+            except IOError as e:
+                continue
+
+def greengrass_infinite_infer_run():
+    try:
+        # Optimizing model (you have to update MxNet with: pip3 install mxnet --upgrade)
+        input_width = 224
+        input_height = 224
+        model_name = "deeplens-asl"
+        error, model_path = mo.optimize(model_name, input_width, input_height)
+        if error:
+            raise Exception(error)
+
+        # Loading model
+        model = awscam.Model(model_path, {"GPU": 1})
+        client.publish(topic=iot_topic, payload="Model loaded")
+
+        # Model variables
+        model_type = "classification"
+        labels = ['a','b','c','d','e','f','g','h','i','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','spok','like','f**k']
+        topk = 1
+
+        # Streaming results
+        results_thread = FIFO_Thread()
+        results_thread.start()
+        client.publish(topic=iot_topic, payload="Inference is starting")
+
+        # Preparing sentence display
+        actual_letter = ''
+        actual_frame_count = 0
+        message = ''
+        minimum_count = 20
+        space_count = 30
+        reset_count = 70
+        total_frame_count = 0
+
+        # Inference loop
+        while True:
+
+            # Retrieving last frame
+            total_frame_count = total_frame_count + 1
+            ret, frame = awscam.getLastFrame()
+            if ret == False:
+                raise Exception("Failed to get frame from the stream")
+
+            # Resizing frame
+            height, width, channels = frame.shape
+            frame_cropped = frame[0:height, (width-height)/2:width-(width-height)/2]
+            frame_resize = cv2.resize(frame_cropped, (input_width, input_height))
+            infer_output = model.doInference(frame_resize)
+            parsed_results = model.parseResult(model_type, infer_output)
+            top_k = parsed_results[model_type][0:topk]
+
+            output_frame = frame_cropped
+
+            # Filtering 40% confidence
+            if (top_k[0]["prob"] > 0.4):
+                # Writing inference results
+                letter = labels[top_k[0]["label"]]
+                if (actual_letter == letter):
+                    actual_frame_count = actual_frame_count + 1
+                    if (actual_frame_count == minimum_count):
+                        message = message + letter
+                else:
+                    actual_frame_count = 1
+                    actual_letter = labels[top_k[0]["label"]]
+            	cv2.putText(output_frame, '{}: {:.2f}'.format(labels[top_k[0]["label"]], top_k[0]["prob"]), (0,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 165, 20), 4)
+            
+                # Publishing results to IoT topic
+                msg = "{"
+    	        prob_num = 0
+            	for obj in top_k:
+                    if prob_num == topk-1:
+                    	msg += '"{}": {:.2f}'.format(labels[obj["label"]], obj["prob"])
+                    else:
+                    	msg += '"{}": {:.2f},'.format(labels[obj["label"]], obj["prob"])
+                    prob_num += 1
+            	msg += "}"
+            	client.publish(topic=iot_topic, payload=msg)
+            else:
+                # Handling whitespaces
+                if (actual_letter != ''):
+                    actual_frame_count = 0
+                    actual_letter = ''
+                else :
+                    actual_frame_count = actual_frame_count + 1
+                    if (actual_frame_count == space_count and not message.endswith(" ")):
+                        message = message + " "
+                    if (actual_frame_count == reset_count):
+                        message = ""
+
+            # Handling caret
+            total_message = message
+            if (total_frame_count % 4 < 2) :
+                total_message = message + "_"
+
+            # Writing sentence
+            cv2.putText(output_frame, total_message, (0,height-50), cv2.FONT_HERSHEY_SIMPLEX, 4, (45, 145, 236), 4)
+            
+            # Streaming frame
+            global jpeg
+            ret, jpeg = cv2.imencode('.jpg', output_frame)
+
+    except Exception as e:
+        msg = "Lambda failed: " + str(e)
+        client.publish(topic=iot_topic, payload=msg)
+
+    # Asynchronously schedule this function to be run again in 15 seconds
+    Timer(15, greengrass_infinite_infer_run).start()
+
+# Execute the function above
+greengrass_infinite_infer_run()
+
+# This is a dummy handler and will not be invoked
+# Instead the code above will be executed in an infinite loop for our example
+def function_handler(event, context):
+    return
